e2c753ba0f3228b156e0283df64ba97946ff3ae4
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-07-29 17:47:44 +0530

    Service section completed.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Aos from 'aos';
 import Header from './components/header/Header';
 import Hero from './components/UI/Hero';
 import Footer from './components/footer/Footer';
+import Services from './components/UI/Services';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Services />
       </main>
       <Footer />
     </>
---
+++ src/components/UI/Services.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+import FrontEndImg from '../../assets/images/front-end.png'
+import BackEndImg from '../../assets/images/front-end.png'
+import UXUI from '../../assets/images/design.png'
+import appsImg from '../../assets/images/apps.png'
+
+
+const aboutDetails = [
+    {
+        heading: "About me",
+        subHeading: `I'm a web developer based in Punjab, originally from Himachal Pradesh. When I'm not coding, you can find me playing the guitar, listening to music, traveling, watching anime, and reading about history. I also love exploring new cafes and trying different cuisines, and weekend getaways are always on my list. My hobbies keep me inspired and help me bring a fresh perspective to my work.
+        `,
+    }
+]
+
+const Services = () => {
+    return (
+        <section id='services'>
+            <div className='container lg:pt-5'>
+                <div className='text-center'>
+                    <h2 className='text-headingColor font-[800] text-[2.4rem] mb-5'>
+                        {aboutDetails[0].heading}
+                    </h2>
+                    <p className='lg:max-w-[800px] lg:mx-auto text-headingColor font-[500] text-[16px] leading-7'>
+                        {/* lg:mx-auto */}
+                        {aboutDetails[0].subHeading}
+                    </p>
+                </div>
+
+                <div className='flex flex-col justify-center sm:py-12'>
+                    <div className='w-full py-3 px-2 sm:max-w-xl sm:mx-auto sm:px-0'>
+                        <div className='relative text-gray-700 antialiased text-sm font-semibold '>
+
+                            {/* Vertical Section Separater */}
+                            <div className='hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 transform-translate-x-1/2'>
+                            </div>
+
+                            {/* Left Section */}
+                            <div className='mt-6 sm:mt-0 sm:mb-12'>
+                                <div className='flex items-center flex-col sm:flex-row'>
+                                    <div className='flex justify-start w-full mx-auto items-center'>
+                                        <div className='w-full sm:w-1/2 sm:pr-8'>
+                                            <div data-aos='fade-right'
+                                                data-aos-duration="1400"
+                                                data-aos-delay="50"
+                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                <h3 className='text-primaryColor font-700 mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                                    Frontend Development
+                                                </h3>
+                                                <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7 ' >
+                                                    As a web developer, I possess a range of technical skills necessary to build and maintain websites. I have expertise in HTML, CSS, and JavaScript, which allows me to create engaging and interactive web pages. My knowledge of CSS frameworks like Bootstrap and Tailwind CSS enables me to create engaging designs efficiently.                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                        transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                            <figure>
+                                                <img src={FrontEndImg} alt="" />
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Right Section */}
+                            <div className='mt-6 sm:mt-0 sm:mb-12'>
+                                <div className='flex items-center flex-col sm:flex-row'>
+                                    <div className='flex justify-end w-full mx-auto items-center'>
+                                        <div className='w-full sm:w-1/2 sm:pl-8'>
+                                            <div data-aos='fade-left'
+                                                data-aos-duration="1400"
+                                                data-aos-delay="50"
+                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                <h3 className='text-primaryColor font-700 mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                                    UI/UX Design
+                                                </h3>
+                                                <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7 ' >
+                                                    I possess a strong understanding of user-centered design principles and have experience in creating intuitive and visually appealing user interfaces for various digital platforms. My proficiency in design tools like Sketch, Figma, and Adobe Creative Suite enables me to create wireframes, prototypes, and high-fidelity mockups efficiently. I also have experience in conducting user research, usability testing, and iterating designs based on user feedback.                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                        transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                            <figure>
+                                                <img src={UXUI} alt="" />
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Left Section */}
+                            <div className='mt-6 sm:mt-0 sm:mb-12'>
+                                <div className='flex items-center flex-col sm:flex-row'>
+                                    <div className='flex justify-start w-full mx-auto items-center'>
+                                        <div className='w-full sm:w-1/2 sm:pr-8'>
+                                            <div data-aos='fade-right'
+                                                data-aos-duration="1400"
+                                                data-aos-delay="50"
+                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                <h3 className='text-primaryColor font-700 mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                                    Backend Development
+                                                </h3>
+                                                <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7 ' >
+                                                    As a web developer, I possess a range of technical skills necessary to build and maintain websites. I have expertise in HTML, CSS, and JavaScript, which allows me to create engaging and interactive web pages. My knowledge of CSS frameworks like Bootstrap and Tailwind CSS enables me to create engaging designs efficiently.                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                        transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                            <figure>
+                                                <img src={BackEndImg} alt="" />
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Right Section */}
+                            <div className='mt-6 sm:mt-0 sm:mb-12'>
+                                <div className='flex items-center flex-col sm:flex-row'>
+                                    <div className='flex justify-end w-full mx-auto items-center'>
+                                        <div className='w-full sm:w-1/2 sm:pl-8'>
+                                            <div data-aos='fade-left'
+                                                data-aos-duration="1400"
+                                                data-aos-delay="50"
+                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                <h3 className='text-primaryColor font-700 mb-3 group-hover:text-white group-hover:font-[600] text-xl'>
+                                                    Application Development
+                                                </h3>
+                                                <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7 ' >
+                                                    I have experience in using several web frameworks, including Django, Flask, and Express.js. I am proficient in building RESTful APIs, handling database operations, and implementing user authentication and authorization using these frameworks. Additionally, I have a good understanding of frontend frameworks like React, enabling me to create full-stack web applications.                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className='rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 
+                                        transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center'>
+                                            <figure>
+                                                <img src={appsImg} alt="" />
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Services;
